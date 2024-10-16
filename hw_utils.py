50c254fa2e5d22edbd8110f4798194828f91d5aa
xialing95 <xialing95@users.noreply.github.com>
2024-10-16 17:44:23 -0400

    update to picamera2

--- hw_utils.py
+++ hw_utils.py
@@ -1,4 +1,4 @@
-import picamera
+import picamera2
 import RPi.GPIO as GPIO
 import time
 import board
--- time_lapse_utils.py
+++ time_lapse_utils.py
@@ -1,4 +1,4 @@
-import picamera
+import picamera2
 import asyncio
 import RPi.GPIO as GPIO
 import os
