bcd26f1d89ec90a9e0617dc26221d818ccd22fe5
xialing95 <xialing95@users.noreply.github.com>
2025-03-03 13:49:23 -0500

    test

--- bme68x_utils.py
+++ bme68x_utils.py
@@ -21,34 +21,34 @@ def get_bme680_data():
             'pressure': sensor.data.pressure,
             'gas_resistance': sensor.data.gas_resistance
         }
-        print(f"Temperature: {data['temperature']:.2f} °C")
-        print(f"Humidity: {data['humidity']:.2f} %")
-        print(f"Pressure: {data['pressure']:.2f} hPa")
-        print(f"Gas Resistance: {data['gas_resistance']} Ohms")
-        print('---')
+        # print(f"Temperature: {data['temperature']:.2f} °C")
+        # print(f"Humidity: {data['humidity']:.2f} %")
+        # print(f"Pressure: {data['pressure']:.2f} hPa")
+        # print(f"Gas Resistance: {data['gas_resistance']} Ohms")
+        # print('---')
         return data
     else:
-        print(f"Temperature: {data['temperature']:.2f} °C")
-        print(f"Humidity: {data['humidity']:.2f} %")
-        print(f"Pressure: {data['pressure']:.2f} hPa")
-        print(f"Gas Resistance: {data['gas_resistance']} Ohms")
-        print('---')
+        # print(f"Temperature: {data['temperature']:.2f} °C")
+        # print(f"Humidity: {data['humidity']:.2f} %")
+        # print(f"Pressure: {data['pressure']:.2f} hPa")
+        # print(f"Gas Resistance: {data['gas_resistance']} Ohms")
+        # print('---')
         print ("Failed to read sensor data")
         return None
 
-# # Example usage
-# if __name__ == "__main__":
-#     try:
-#         while True:
-#             sensor_data = get_bme680_data()
-#             if sensor_data:
-#                 print(f"Temperature: {sensor_data['temperature']:.2f} °C")
-#                 print(f"Humidity: {sensor_data['humidity']:.2f} %")
-#                 print(f"Pressure: {sensor_data['pressure']:.2f} hPa")
-#                 print(f"Gas Resistance: {sensor_data['gas_resistance']} Ohms")
-#                 print('---')
-#             else:
-#                 print("Failed to read sensor data.")
-#             time.sleep(1)  # Read every second
-#     except KeyboardInterrupt:
-#         print("Exiting...")
+# Example usage
+if __name__ == "__main__":
+    try:
+        while True:
+            sensor_data = get_bme680_data()
+            if sensor_data:
+                print(f"Temperature: {sensor_data['temperature']:.2f} °C")
+                print(f"Humidity: {sensor_data['humidity']:.2f} %")
+                print(f"Pressure: {sensor_data['pressure']:.2f} hPa")
+                print(f"Gas Resistance: {sensor_data['gas_resistance']} Ohms")
+                print('---')
+            else:
+                print("Failed to read sensor data.")
+            time.sleep(1)  # Read every second
+    except KeyboardInterrupt:
+        print("Exiting...")
