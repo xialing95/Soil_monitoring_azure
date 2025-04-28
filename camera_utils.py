8c0a44035706bfcc0781ca0d9f8db047af31259b
xialing95 <xialing95@users.noreply.github.com>
2025-04-28 16:47:51 -0400

    resolution fix

--- camera_utils.py
+++ camera_utils.py
@@ -40,6 +40,13 @@ def update_log(filename: str):
     log_f.close()
     return
 
+#Check disk space (if it is less than 2mb, it will not be able to save the image)
+def check_disk_space(directory, required_space_mb=200):
+    """Check if there is enough disk space in the specified directory."""
+    statvfs = os.statvfs(directory)
+    free_space_mb = (statvfs.f_bavail * statvfs.f_frsize) / (1024 * 1024)
+    return free_space_mb >= required_space_mb
+
 def preview():
     laser_on()
     time.sleep(3)
@@ -80,25 +87,25 @@ def preview():
         laser_off()  # Ensure laser always turns off
     return
 
-def capture_image(camera, path):
-    """Capture an image and save it to the specified path."""
-    camera.capture_file(path)
-    print(f"Captured image: {path}")
-    return
-
-#Check disk space (if it is less than 2mb, it will not be able to save the image)
-def check_disk_space(directory, required_space_mb=200):
-    """Check if there is enough disk space in the specified directory."""
-    statvfs = os.statvfs(directory)
-    free_space_mb = (statvfs.f_bavail * statvfs.f_frsize) / (1024 * 1024)
-    return free_space_mb >= required_space_mb
-
 # Function to capture images
 def capture_timelapse(interval, duration, NAME):
+    # Load camera settings from JSON
+    with open('CAMERASETTING.json', 'r') as f:
+        config = json.load(f)
+    
+    # Get resolution from config (default to 1920x1080)
+    resolution = tuple(config.get("resolution", [1920, 1080]))
+
     from picamera2 import Picamera2
 
     with Picamera2() as picam2:
+        # Configure camera with resolution
+        camera_config = picam2.create_still_configuration(
+            main={"size": resolution}
+        )
+        picam2.configure(camera_config)
         picam2.start()
+        
         frame_count = 0
         expected_frame_count = int(duration / interval)
 
@@ -128,54 +135,6 @@ def capture_timelapse(interval, duration, NAME):
             print(f"Warning: Did not capture expected number of images.")
 
         laser_off()
-
-# def capture_timelapse(interval, duration, NAME):
-#     picam2 = Picamera2()
-#     picam2.start()  # Start the camera
-#     frame_count = 0
-#     expected_frame_count = int(duration/interval)
-
-#     if not check_disk_space(APP_STATIC):
-#         print("Not enough disk space to save images. Minimal 200mb")
-#         return
-
-#     # second from epoch time + duration in seconds
-#     start_time = time.time()
-#     while (time.time()-start_time) < duration:
-#         #turn laser on
-#         laser_on()
-
-#         # Get the current time to create the name of the file
-#         timestr = time.strftime("%m-%d-%H%M%S", time.localtime())
-#         imageName = f"{timestr}-{NAME}" 
-#         path = os.path.join(APP_STATIC, imageName)
-
-#         # Capture both DNG and JPG images
-#         request = picam2.capture_request()
-#         request.save("main", 'static/preview.jpg')
-#         print(f"file name: {imageName}")
-                
-#         # Save raw image data
-#         # raw_array = request.make_array("raw")
-#         # raw_data = np.save(path, raw_array)
-#         # np.save(path, raw_data)
-
-#         request.save_dng(path)
-#         frame_count += 1
-
-#         request.release()  # Release the request
-#         time.sleep(interval)  # Wait for the specified interval
-
-#     if frame_count == expected_frame_count:
-#         print ("Success! Captured the expected number of images.")
-#     else: 
-#         print(f"Warning: Did not capture expect number of images.")
-    
-#     picam2.stop()  # Stop the camera
-#     del picam2
-#     print ("Stop the picamera")
-#     laser_off()
-#     return 
 
 # Time Lapse Main function
 def time_lapse(inputDuration, inputInterval, NAME):
