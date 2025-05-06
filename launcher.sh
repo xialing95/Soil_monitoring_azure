aa2484f5736265da01b81c36340baab03218d3be
xialing95 <xialing95@users.noreply.github.com>
2025-05-06 14:56:20 -0400

    launcher fix

--- launcher.sh
+++ launcher.sh
@@ -2,4 +2,4 @@ kill -9 $(lsof -t -i:5000)
 cd /home/pi/Soil_monitoring_azure
 source env/bin/activate
 PYTHONPATH=/home/pi/env/bin/python
-python3 /home/pi/soil_azure/hotspot.py
+python3 /home/pi/Soil_monitoring_azure/hotspot.py
